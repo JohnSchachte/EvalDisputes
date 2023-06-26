c6bf9e0c9368ad779b7f3afba63da53e33c5e584
John Schachte <johnpete@ad.unc.edu>
2023-06-26 12:52:11 -0400

    restructure

---
+++ ApprovalTasks/HasCoachingBackend.js
@@ -0,0 +1,14 @@
+class HasCoachingBackend extends ApprovalTask {
+    constructor(name,process) {
+        super(name,process,JSON.stringify([name,process.rootKey]));
+    }
+
+    run(){
+        const reader = Custom_Utilities.getMemoizedReads(cache);
+        const formResponse = reader(BACKEND_ID,`Submissions!${this.process.rootKey}:${this.process.rootKey}`).values[0]; 
+        const colMap = mkColMap(reader(BACKEND_ID,"Submissions!1:1").values[0]);
+        const email = formResponse[colMap.get("Email Address")];
+        const hasBackend = OperationCoachingMembers.isInEmailSet(formResponse[colMap.get("Email Address")].toLowerCase());
+        return hasBackend ? false : [email,`Your WFM team ${EmailToWFM.getAgentObj["Team"]} does not map to a backend to store the data. Please, reach out to your supervisor`];
+    }
+}
--- aaa/aaTask.js
+++ aaa/aaTask.js
@@ -1,4 +1,13 @@
+/**
+ * Task class that represents a task in the process.
+ */
 class Task {
+  /**
+   * Constructs a new Task instance.
+   * @param {string} name - The name of the task.
+   * @param {Process} process - The process that this task belongs to.
+   * @param {string} taskKey - The key of the task.
+   */
     constructor(name,process,taskKey) {
       this.parents = new Map();
       this.children = new Map();
@@ -9,76 +18,122 @@ class Task {
       this.ss =  SpreadsheetApp.openById(BACKEND_ID); // make this the id of the spreadsheet of information
 
     }
-
+  /**
+  * Retrieves a node by its name from children, siblings or parents.
+  * @param {string} name - The name of the node.
+  * @returns {Node} The node.
+  */
     getNode(name){
       return (this.children.get(name) || this.siblings.get(name) || this.parents.get(name));
     }
-    
+    /**
+    * Returns the name of the task.
+    * @returns {string} The name of the task.
+    */
     getName(){return this.name;}
-
+  /**
+  * Sets a sibling for the task.
+  * @param {Task} node - The sibling task.
+  */
     setSibling(node){
       Logger.log("setting %s as sibling to %s",node.getName(),this.name);
       this.siblings.set(node.getName(),node);
     }
-
+    /**
+    * Sets a child for the task.
+    * @param {Task} node - The child task.
+    */
     setChild(node){
       this.children.set(node.getName(),node);
     }
 
+     /**
+    * Sets multiple siblings for the task.
+    * @param {Array<Task>} nodes - The sibling tasks.
+    */
     setSiblings(nodes){
       for(let node of nodes){
         this.setSibling(node);
       }
     }
-
+ /**
+  * Sets multiple children for the task.
+  * @param {Array<Task>} nodes - The child tasks.
+  */
     setChildren(nodes){
       for(let node of nodes){
         this.setChild(node);
       }
-    }
-
+    }.
+ /**
+  * Updates the state of the process.
+  * @param {string} state - The new state.
+  */
     updateProcess(state){
         this.process.setState(state);
     }
-
+  /**
+    * Sets a trigger for the task. To be implemented in child classes.
+    */
     setTriggerSelf() {
       // Implement this method in child classes
       throw new Error('You have to implement the method setTriggerSelf!');
     }
-  
+   /**
+  * Fires a trigger for the task. To be implemented in child classes.
+  */
     fireTriggerSelf() {
       // Implement this method in child classes
       throw new Error('You have to implement the method fireTriggerSelf!');
     }
-  
+   /**
+  * Logs the task. To be implemented in child classes.
+  */
     logSelf() {
       // Implement this method in child classes
       throw new Error('You have to implement the method logSelf!');
     }
-  
+   /**
+  * Updates the state of the task.
+  * @param {string} newState - The new state.
+  */
     updateSelfState(newState) {
       this.process.storage.set(this.taskKey+"state",newState);
     }
-  
+   /**
+  * Returns the state of the task.
+  * @returns {string} The state of the task.
+  */
     getStateSelf() {
       return this.process.storage.get(this.taskKey+"state");
     }
-  
+   /**
+  * Handles the success scenario. To be implemented in child classes.
+  */
     onSuccess() {
       // Implement this method in child classes
       throw new Error('You have to implement the method onSuccess!');
     }
-  
+  /**
+  * Handles the failure scenario. To be implemented in child classes.
+  */
     onFailure() {
       // Implement this method in child classes
       throw new Error('You have to implement the method onFailure!');
     }
-
+ /**
+  * Deconstructs the task. To be implemented in child classes.
+  */
     deconstruct(){
         //Implement this in child classes
         throw new Error('You have to implement the method deconstruct!');
     }
-
+ /**
+  * Checks the state of the neighbors.
+  * @param {Array<Task>} neighbors - The neighbor tasks.
+  * @param {string} state - The state to check.
+  * @returns {boolean} True if all neighbors are in the given state, false otherwise.
+  */
     checkNeighborsState(neighbors,state){
       let result = true;
       neighbors.forEach(neighbor => {
@@ -90,13 +145,21 @@ class Task {
       });
       return result;
     }
-
-    deconstructNeighbors(neighbors,condition = (neighbor)=>true){
-        for(let neighbor of neighbors){
-            if(condition(neighbor)) neighbor.deconstruct();
+  /**
+  * Deconstructs the neighbor tasks.
+  * @param {Array<Task>} neighbors - The neighbor tasks.
+  * @param {conditionCallback} condition - The condition to check for each neighbor.
+  */
+    deconstructNeighbors(neighbors, condition = (neighbor) => true) {
+      neighbors.forEach(neighbor => {
+        if(condition(neighbor)) {
+          neighbor.deconstruct();
         }
+      });
     }
-
+     /**
+  * Runs the task. To be implemented in child classes.
+  */
     run(){
         //Implement this in child classes
         throw new Error('You have to implement the method run!');
