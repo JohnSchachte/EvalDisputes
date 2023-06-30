35a031bed715efcb30b05a391bfd8727629954aa
John Schachte <johnpete@ad.unc.edu>
2023-06-30 14:58:20 -0400

    added garbage collection

--- Utils/Storage.js
+++ Utils/Storage.js
@@ -13,3 +13,16 @@ function Storage() {
   Storage.prototype.remove = function(key) {
     this.storage.deleteProperty(key);
   }
+
+  Storage.prototype.garbageCollect = function() {
+    var properties = this.storage.getAllProperties();
+  
+    // Create an array of keys
+    var keys = Object.keys(properties);
+  
+    // Filter keys based on the condition
+    var keysToDelete = keys.filter((key) => properties[key] === 'success' || properties[key] === 'denied');
+  
+    // Delete the properties
+    keysToDelete.forEach(key =>  this.storage.deleteProperty(key));  // passing `this` for correct context inside forEach
+  }
